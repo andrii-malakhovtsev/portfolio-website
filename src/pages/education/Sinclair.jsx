8e7188cd28c65e83e96bb2d3ac6c0c70dddc3753
andrii-malakhovtsev <malakhovtsevandrii@gmail.com>
2025-06-17 00:14:27 -0400

    #22 separate section for courses with links

--- src/pages/education/Sinclair.jsx
+++ src/pages/education/Sinclair.jsx
@@ -27,59 +27,67 @@ export default function Sinclair() {
           <h2>Programming Coursework</h2>
         
           <div class="class">
-            <h3>CIS 2222 – ASP.NET with C#</h3>
+            <h3>ASP.NET with C#</h3>
             <p>Introduces server side web programming to develop web applications based on ASP.NET.</p>
             <p><strong>Technologies:</strong> C#, ASP.NET Core</p>
+            <p><strong>Course:</strong> <a href="https://www.sinclair.edu/course/params/subject/CIS/courseNo/2222/" class="doc-icon" target="_blank">CIS 2222</a></p>
             <a href="https://github.com/andrii-malakhovtsev/quarterly-sales" class="github-icon" target="_blank"><i class="fab fa-github"></i></a>
           </div>
 
           <div class="class">
-            <h3>CIS 2207 – Data Structures & Algorithms</h3>
+            <h3>Data Structures & Algorithms</h3>
             <p>Covers data abstraction, encapsulation, recursion, and manipulation of data structures like lists, queues, trees, heaps, and graphs using C++ programming.</p>
             <p><strong>Technologies:</strong> C++</p>
+                        <p><strong>Course:</strong> <a href="https://www.sinclair.edu/course/params/subject/CIS/courseNo/2207/" class="doc-icon" target="_blank">CIS 2207</a></p>
             <a href="https://github.com/andrii-malakhovtsev/cis-2207" class="github-icon" target="_blank"><i class="fab fa-github"></i></a>
           </div>
 
           <div class="class">
-            <h3>CIS 2165 – Database Management</h3>
+            <h3>Database Management</h3>
             <p>Design and develop a simple database and implement a portion of this application including forms, queries and reports. Emphasis on database design techniques, normalization and the SQL database language.</p>
             <p><strong>Technologies:</strong> SQL, MS Access</p>
+            <p><strong>Course:</strong> <a href="https://www.sinclair.edu/course/params/subject/CIS/courseNo/2165/" class="doc-icon" target="_blank">CIS 2165</a></p>
           </div>
         
           <div class="class">
-            <h3>CIS 2217 – Java Software Development II</h3>
+            <h3>Java Software Development II</h3>
             <p>Focuses on abstract classes, interfaces, binary I/O, recursion, and implementing data structures such as lists, stacks, queues, and priority queues.</p>
             <p><strong>Technologies:</strong> Java</p>
+            <p><strong>Course:</strong> <a href="https://www.sinclair.edu/course/params/subject/CIS/courseNo/2217/" class="doc-icon" target="_blank">CIS 2217</a></p>
             <a href="https://github.com/andrii-malakhovtsev/cis-2217" class="github-icon" target="_blank"><i class="fab fa-github"></i></a>
           </div>
         
           <div class="class">
-            <h3>CIS 1202 – C++ Software Development</h3>
+            <h3>C++ Software Development</h3>
             <p>Introduces arrays, searching and sorting algorithms, pointers, structures, file operations, classes, inheritance, polymorphism, and virtual functions in C++.</p>
             <p><strong>Technologies:</strong> C++</p>
+            <p><strong>Course:</strong> <a href="https://www.sinclair.edu/course/params/subject/CIS/courseNo/1202/" class="doc-icon" target="_blank">CIS 1202</a></p>
             <a href="https://github.com/andrii-malakhovtsev/cis-1202" class="github-icon" target="_blank"><i class="fab fa-github"></i></a>
           </div>
         
           <div class="class">
-            <h3>CIS 2212 – Java Software Development I</h3>
+            <h3>Java Software Development I</h3>
             <p>Covers object-oriented programming concepts, Java syntax, data types, control structures, file processing, GUI applications, arrays, and ArrayList class.</p>
             <p><strong>Technologies:</strong> Java</p>
+            <p><strong>Course:</strong> <a href="https://www.sinclair.edu/course/params/subject/CIS/courseNo/2212/" class="doc-icon" target="_blank">CIS 2212</a></p>
             <a href="https://github.com/andrii-malakhovtsev/cis-2212" class="github-icon" target="_blank"><i class="fab fa-github"></i></a>
           </div>
         
           <div class="class">
-            <h3>CIS 1350 – Web Site Development with HTML & CSS</h3>
+            <h3>Web Site Development with HTML & CSS</h3>
             <p>Teaches creation of websites using HTML and CSS, emphasizing current web standards, validation tools, and cross-browser compatibility.</p>
             <p><strong>Technologies:</strong> HTML, CSS</p>
+            <p><strong>Course:</strong> <a href="https://www.sinclair.edu/course/params/subject/CIS/courseNo/1350/" class="doc-icon" target="_blank">CIS 1350</a></p>
             <a href="https://github.com/andrii-malakhovtsev/cis-1350-final-project" class="github-icon" target="_blank"><i class="fab fa-github"></i></a>
           </div>
         
           <div class="class">
-            <h3>CIS 1111 – Introduction to Problem Solving & Computer Programming</h3>
+            <h3>Introduction to Problem Solving & Computer Programming</h3>
             <p>Introduces problem-solving techniques using flowcharts and pseudocode, and programming fundamentals in C++ including sequence, selection, iteration, functions, and arrays.</p>
             <p><strong>Technologies:</strong> C++</p>
+            <p><strong>Course:</strong> <a href="https://www.sinclair.edu/course/params/subject/CIS/courseNo/1111/" class="doc-icon" target="_blank">CIS 1111</a></p>
           </div>
-        </section>    
+        </section>
       </main>
     </>
   )
